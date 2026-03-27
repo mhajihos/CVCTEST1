189ba38271638b0c9ed021955988556bcded7985
mhajihos <mhajihos@ualberta.ca>
2026-03-27 11:44:44 -0600

    commit

--- my_summary.R
+++ my_summary.R
@@ -1,3 +1,13 @@
 my_summary <- function(x) {
-  mean(x)
+  if (!is.numeric(x)) {
+    stop("Input must be numeric")
+  }
+  
+  x <- na.omit(x)
+  
+  data.frame(
+    mean = mean(x),
+    median = median(x),
+    sd = sd(x)
+  )
 }
